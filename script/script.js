80848ac641bb118ef39db2d3af8d972c00a6ae25
Gabrie1Garcia <90660755+Gabrie1Garcia@users.noreply.github.com>
2022-05-06 11:42:55 -0300

    add

---
+++ script/script.js
@@ -0,0 +1,18 @@
+let menuToggle = document.querySelector('.toggle');
+let navigation = document.querySelector('.navigation')
+let sec = document.querySelector('.sec')
+menuToggle.onclick = function () {
+    menuToggle.classList.toggle('active')
+    navigation.classList.toggle('active')
+    sec.classList.toggle('active')
+}
+
+function show() {
+    document.querySelector('.hiddenD').style.display = 'block'; 
+}
+
+function hide() {
+    document.querySelector('.hiddenD').style.display = 'none'; 
+}
+
+
